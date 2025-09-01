e035060609ce1030e1e33ac528f70a35bfca1495
Will-McBurney <pmcburn1@gmail.com>
2025-08-31 23:16:10 -0400

    Updated starter file to allow for Kotlin or Java

--- src/main/java/edu/nd/sde/hw1/HelloJava.java
+++ src/main/java/edu/nd/sde/hw1/HelloJava.java
@@ -1,6 +1,6 @@
-package edu.virginia.sde.hw1;
+package edu.nd.sde.hw1;
 
-public class HelloWorld {
+public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
---
+++ src/main/java/edu/nd/sde/hw1/HelloKotlin.kt
@@ -0,0 +1,5 @@
+package edu.nd.sde.hw1
+
+fun main() {
+    println("Hello Kotlin")
+}
